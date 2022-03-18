abfa02887514e662732bf0fd59111468676c5bfa
Kayne <60493018+Kayne19@users.noreply.github.com>
2022-03-18 07:56:13 -0700

    First Commit

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -2,6 +2,10 @@ module.exports.renderAccounting = function(req, res){
     res.render('pages/accounting');
 }
 module.exports.renderDashboard = function(req, res){
+    if (!req.user.can(action)) {
+        res.render('/');
+        return
+    }
     res.render('pages/dashboard');
 }
 module.exports.renderHR = function(req, res){
@@ -12,5 +16,8 @@ module.exports.renderMarketing = function(req, res){
 }
 module.exports.renderSales= function(req, res){
     res.render('pages/sales');
+}
+module.exports.viewProfile= function(req,res){
+    res.render('pages/profile');
 }
 
